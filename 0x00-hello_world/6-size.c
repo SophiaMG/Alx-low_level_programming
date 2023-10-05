b1e1cf446a45a8e798eaa89e50232ff7f1b63fff
SophiaMG <sophiexmakonnen@gmail.com>
2023-10-05 08:36:19 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the starting point of the program
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)," sizeof(char));
+	printf("Size of an int:%lu byte(s)," sizeof(int));
+	printf("Size of a long int:%lu byte(s)," sizeof(long int));
+	printf("Size of a long long int:%lu byte(s)," sizeof(long long int));
+	return (0);
+}
