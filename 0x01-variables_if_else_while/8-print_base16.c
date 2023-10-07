99f709993ea53ec6c527e6101226ab9e0d2c5162
SophiaMG <sophiexmakonnen@gmail.com>
2023-10-07 10:35:59 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
+
+
