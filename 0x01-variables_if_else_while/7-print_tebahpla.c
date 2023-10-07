fa33b468d6b8f8294b723dbcebc2463d9d57a683
SophiaMG <sophiexmakonnen@gmail.com>
2023-10-07 10:22:19 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
